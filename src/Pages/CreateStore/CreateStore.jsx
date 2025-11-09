eb7c21c5f06ca07d031c3193d08c86d8499378d7
Diamond_TECH <ezehemmanuel200118@gmail.com>
2025-11-09 16:21:46 +0000

    y

--- src/Pages/CreateStore/CreateStore.jsx
+++ src/Pages/CreateStore/CreateStore.jsx
@@ -98,7 +98,7 @@ useEffect(() => {
     setStoreLink(publicLink);
   } catch (err) {
     setType("error");
-    setMsg(err.response?.data?.msg || "Failed to create store");
+    setMsg(err.response?.data?.message || "Failed to create store");
   } finally {
     setTimeout(() => setMsg(""), 6000);
     setLoading(false);
